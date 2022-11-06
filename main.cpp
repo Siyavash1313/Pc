3fd382df8c4d38e82590e739044ec235a0e01d57
Siyavash1313 <117150145+Siyavash1313@users.noreply.github.com>
2022-11-06 14:00:35 +0330

    Pcp42

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-#include <iostream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */ 
 
- using namespace std;	
- int main() {
-int x,y,z;
-  float ans;
-  cout <<"enter x value:";
-    cin >> x;
-      cout << "\n enter y value:";
-    cin >> y;
-    cout <<"\n enter z value:";
-    cin >> z;
-    ans = (x-y)*(x+z)/(x+y+z)*(x+y+z)+1;
-    cout << ans;
-}
+  using namespace std;
+   	
+  int main() {
 
-
-
+ float  a,b,jam,tafrig,zarb,tagsim;
+   cout << "enter number one: \n";
+   cin>> a;
+   cout << "enter number two: \n";
+   cin >> b;
+   jam = a+b;
+   cout <<"jam:" << jam << "\n";
+   tafrig = a-b;
+   cout <<"tafrig:" << tafrig << "\n";
+   zarb = a*b;
+   cout <<"zarb:" << zarb << "\n";
+   tagsim = a/b;
+   cout <<"tagsim:" << tagsim << "\n";
+  }
+	
