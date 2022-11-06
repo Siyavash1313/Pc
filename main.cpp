5e0a9dae531961656c4c248459f18d15ede66cf6
Siyavash1313 <117150145+Siyavash1313@users.noreply.github.com>
2022-11-06 13:37:49 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */ 
+
+  using namespace std;
+   	
+  int main() {
+
+ float  a,b,jam,tafrig,zarb,tagsim;
+   cout << "enter number one: \n";
+   cin>> a;
+   cout << "enter number two: \n";
+   cin >> b;
+   jam = a+b;
+   cout <<"jam:" << jam << "\n";
+   tafrig = a-b;
+   cout <<"tafrig:" << tafrig << "\n";
+   zarb = a*b;
+   cout <<"zarb:" << zarb << "\n";
+   tagsim = a/b;
+   cout <<"tagsim:" << tagsim << "\n";
+  }
+	
