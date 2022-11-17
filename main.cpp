c4a53d8f9719b6d73e4c687b0c9d94363d1e118f
Siyavash1313 <117150145+Siyavash1313@users.noreply.github.com>
2022-11-17 12:41:49 +0330

    Cp43

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */ 
+using namespace std;
 
-  using namespace std;
-   	
-  int main() {
-
- float  a,b,jam,tafrig,zarb,tagsim;
-   cout << "enter number one: \n";
-   cin>> a;
-   cout << "enter number two: \n";
-   cin >> b;
-   jam = a+b;
-   cout <<"jam:" << jam << "\n";
-   tafrig = a-b;
-   cout <<"tafrig:" << tafrig << "\n";
-   zarb = a*b;
-   cout <<"zarb:" << zarb << "\n";
-   tagsim = a/b;
-   cout <<"tagsim:" << tagsim << "\n";
-  }
+int main() {
 	
+  int sps;
+ cout << "enter the sps:";
+  cin >> sps;
+  int texpercentage;
+  if (sps >= 25000000)
+  texpercentage = 35;
+  else if (sps >= 18000000)
+  texpercentage = 25;
+  else if (sps >= 14000000)
+  texpercentage = 20;
+  else if (sps >= 10000000)
+  texpercentage = 15;
+  else if (sps >= 8000000)
+  texpercentage = 10;
+  else if (sps >= 6000000)
+  texpercentage = 5;
+  else texpercentage = 0;
+  int spsAfterTask = (sps*(100-texpercentage))/100;
+  cout << "sps after tax: \n" << spsAfterTask;
+  return 0;
+
+}
+
+
